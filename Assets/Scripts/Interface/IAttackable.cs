b2cd08129e02bf84feca10fedb22b1c93aa7f5a9
고재연 <kojy2004@yonsei.ac.kr>
2024-07-11 11:59:08 +0900

    [Init] 인터페이스 추가

---
+++ Assets/Scripts/Interface/IAttackable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAttackable
+{
+    public void OnTakeDamaged<T>(T damage);
+    public void OnTakeDebuffed<T>(T debuff);
+    public void OnTakebuffed<T>(T buffed);
+}
