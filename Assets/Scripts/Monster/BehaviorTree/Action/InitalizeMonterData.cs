79c3767ef472b4141d0ee733822e33c15fcf9c39
고재연 <kojy2004@yonsei.ac.kr>
2024-07-12 11:00:31 +0900

    [Update] BT - Variable 공유

--- Assets/Scripts/Monster/BehaviorTree/Action/InitalizeMonterData.cs
+++ Assets/Scripts/Monster/BehaviorTree/Action/InitalizeMonterData.cs
@@ -1,12 +1,22 @@
+using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine;
 
 [TaskCategory("Monster/Initalize")]
 public class InitalizeMonterData : Action
 {
+    public SharedTransform TargetTrans;
     public override TaskStatus OnUpdate()
     {
-        Debug.Log("InitalizeMonterData_OnUpdate");
-        return TaskStatus.Success;
+        var targetObject = GameObject.FindGameObjectWithTag("Player");
+        if (targetObject == null)
+        {
+            return TaskStatus.Failure;
+        }
+        else
+        {
+            TargetTrans.Value = targetObject.transform;
+            return TaskStatus.Success;
+        }
     }
 }
