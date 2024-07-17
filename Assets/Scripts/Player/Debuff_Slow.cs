8c9c840b84cdb65f45b561e8e6af4ae4b295d0d9
고재연 <kojy2004@yonsei.ac.kr>
2024-07-17 10:05:54 +0900

    [Update] 물길 수정 완료

--- Assets/Scripts/Player/Debuff_Slow.cs
+++ Assets/Scripts/Player/Debuff_Slow.cs
@@ -7,7 +7,7 @@ public class Debuff_Slow : Debuff
     private float originalMoveSpeed;
     private float slowPercentage;
 
-    public Debuff_Slow(float slowPercentage) : base(0, 0, 0) // 지속 시간 없음
+    public Debuff_Slow(float slowPercentage) : base(1f, 0, 0) // 지속 시간 없음
     {
         this.slowPercentage = slowPercentage;
     }
@@ -18,14 +18,20 @@ public class Debuff_Slow : Debuff
         if (player != null)
         {
             originalMoveSpeed = player.MoveSpeed;
-            player.MoveSpeed *= (1 - slowPercentage / 100f);
+            player.MoveSpeed *= (1 - slowPercentage);
         }
         else
         {
             Monster monster = target.GetComponent<Monster>();
             if (monster != null)
             {
-                monster.bt.SetVariableValue("Speed", monster.monster_Data.MoveSpeed/2);
+                monster.bt.SetVariableValue("Speed", monster.monster_Data.MoveSpeed * (1 - slowPercentage));
+                if (monster is Monster_Elite)
+                {
+                    var monsterE = monster as Monster_Elite;
+                    var data = monsterE.monster_Data as Monster_Data_Elite;
+                    monsterE.bt.SetVariableValue("Skill2Speed", data.skill_footWalk_Speed * (1 - slowPercentage));
+                }
             }
         }
     }
@@ -43,7 +49,12 @@ public class Debuff_Slow : Debuff
             if (monster != null)
             {
                 monster.bt.SetVariableValue("Speed", monster.monster_Data.MoveSpeed);
-                Debug.Log("Remove");
+                if(monster is Monster_Elite)
+                {
+                    var monsterE = monster as Monster_Elite;
+                    var data = monsterE.monster_Data as Monster_Data_Elite;
+                    monsterE.bt.SetVariableValue("Skill2Speed", data.skill_footWalk_Speed);
+                }
             }
         }
     }
